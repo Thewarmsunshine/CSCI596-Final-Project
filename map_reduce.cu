1d3c9ba8fd63f8c611ba1818e73e0170a1372043
Jiayi(Kevin) Liu <56602568+Thewarmsunshine@users.noreply.github.com>
2023-12-12 23:49:56 -0800

    Update map_reduce.cu

--- map_reduce.cu
+++ map_reduce.cu
@@ -11,12 +11,12 @@
 
 using namespace std;
 
-extern __device__ void mapper(input_type *input, KeyValuePair *pairs);
-extern __device__ void reducer(KeyValuePair *pairs, int len, output_type *output);
 
-/*
- * Macro to check for GPU errors
- */
+// 从外部引入的 mapper 和 reducer 函数
+extern __device__ void mapper(input_type *input, Pair *pairs);
+extern __device__ void reducer(Pair *pairs, int len, output_type *output);
+
+// GPU 错误检查宏
 #define gpuErrChk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(
     cudaError_t code,
@@ -31,156 +31,83 @@ inline void gpuAssert(
     }
 }
 
-/*
- * An operator definition that allows comparisons between two KeyValuePairs
- * using the StrictWeakOrdering binary predicate. This does a byte by byte
- * comparison of the key, and returns True if the first pair has a key less than
- * second pair.
- */
-struct keyValueCompare {
-    __host__ __device__ bool operator() (const KeyValuePair &lhs, const KeyValuePair &rhs) {
-        void *char_lhs = (unsigned char *) &(lhs.key);
-        void *char_rhs = (unsigned char *) &(rhs.key);
-        for (int i = 0; i < sizeof(key_type); i++) {
-            unsigned char *p1 = (unsigned char *) char_lhs + i;
-            unsigned char *p2 = (unsigned char *) char_rhs + i;
-            if (*p1 < *p2) {
-                return true;
-            } else if (*p1 > *p2) {
-                return false;
-            }
-        }
-        return false;
-    }
-};
+// 声明 CUDA map 和 reduce 函数
+void cudaMap(input_type *input, Pair *pairs);
+void cudaReduce(Pair *pairs, output_type *output);
 
-// Declare mapper and reducer functions
-void cudaMap(input_type *input, KeyValuePair *pairs);
-void cudaReduce(KeyValuePair *pairs, output_type *output);
-
-/*
- * Mapping Kernel: Since each mapper runs independently of each other, we can
- * give each thread its own input to process and a disjoint space where it can`
- * store the key/value pairs it produces.
- */
-__global__ void mapKernel(input_type *input, KeyValuePair *pairs) {
+// map 内核函数：每个线程处理一个输入元素
+__global__ void mapKernel(input_type *input, Pair *pairs) {
     for (size_t i = blockIdx.x * blockDim.x + threadIdx.x;
         i < NUM_INPUT;
         i += blockDim.x * gridDim.x) {
-        mapper(&input[i], &pairs[i * NUM_KEYS]);
+        mapper(&input[i], &pairs[i]);
     }
 }
 
-/*
- * Reducing Kernel: Given a sorted array of keys, find the range corresponding
- * to each thread and run the reducer on that set of key/value pairs.
- */
-__global__ void reduceKernel(KeyValuePair *pairs, output_type *output) {
+// reduce 内核函数：处理排序后的 Pair 对象并运行 reducer
+__global__ void reduceKernel(Pair *pairs, output_type *output) {
     for (size_t i = blockIdx.x * blockDim.x + threadIdx.x;
         i < NUM_OUTPUT;
         i += blockDim.x * gridDim.x) {
-        int startIndex = 0;
-        int count = 0;
         int valueSize = 0;
-        int j;
 
-        for (j = 1; j < NUM_INPUT * NUM_KEYS; j++) {
-            if (keyValueCompare()(pairs[j - 1], pairs[j])) {
-                if (count == i) {
-                    // This thread has found the correct number
-                    // There is a bit of warp divergence here as some threads
-                    // break before others, but we still make the most out of it
-                    // by calling the reducer at the very end, so there is not
-                    // any warp divergence where the bulk of the computation
-                    // should occur (the reducer).
-                    break;
-                } else {
-                    count++;
-                    startIndex = j;
-                }
-            }
-        }
+        valueSize = NUM_INPUT; // 每个输出都基于所有输入计算
 
-        if (count < i) {
-            // This thread doesn't need to process a key. We won't get here, but
-            // this code is just there for assurance.
-            return;
-        }
-
-        valueSize = j - startIndex;
-
-        // Run the reducer
-        reducer(pairs + startIndex, valueSize, &output[i]);
+        reducer(pairs, valueSize, &output[i]);
     }
 }
 
-/*
- * The main function that runs the bulk of the MapReduce job. Space is allocated
- * on the GPU, inputs are copied. The mapper is run. The key/value pairs are
- * sorted. The reducer is run. Output data is copied back from the GPU and
- * returned.
- */
+// 主函数：运行整个 MapReduce 作业
 void runMapReduce(input_type *input, output_type *output) {
-    // Create device pointers
     input_type   *dev_input;
     output_type  *dev_output;
-    KeyValuePair *dev_pairs;
+    Pair *dev_pairs;
 
-    // Determine sizes in bytes
+    // 计算内存大小
     size_t input_size = NUM_INPUT * sizeof(input_type);
     size_t output_size = NUM_OUTPUT * sizeof(output_type);
-    size_t pairs_size = NUM_INPUT * NUM_KEYS * sizeof(KeyValuePair);
+    size_t pairs_size = NUM_INPUT * sizeof(Pair);
 
-    // Initialize device memory (we can utilize more space by waiting to
-    // initialize the output array until we're done with the input array)
+    // 在 GPU 上初始化内存
     cudaMalloc(&dev_input, input_size);
     cudaMalloc(&dev_pairs, pairs_size);
 
-    // Copy input data over
+    // 将输入数据复制到 GPU
     cudaMemcpy(dev_input, input, input_size, cudaMemcpyHostToDevice);
-    //cudaMemset(dev_pairs, 0, pairs_size);
 
-    // Run the mapper kernel
+
+    // 运行 map 内核
     cudaMap(dev_input, dev_pairs);
 
-    // Convert the pointer to device memory for the key/value pairs that is
-    // recognizable by the cuda thrust library
-    thrust::device_ptr<KeyValuePair> dev_ptr(dev_pairs);
+    // 使用 Thrust 对 Pair 对象进行排序
+    thrust::device_ptr<Pair> dev_ptr(dev_pairs);
+    thrust::sort(dev_ptr, dev_ptr + NUM_INPUT);
 
-    // Sort the key/value pairs. By using the thrust library, we don't have to
-    // write this code ourselves, and it's already optimized for parallel
-    // computation
-    thrust::sort(dev_ptr, dev_ptr + NUM_INPUT * NUM_KEYS, keyValueCompare());
-
-    // Free GPU space for the input
+    // 释放输入数据所用的 GPU 空间
     cudaFree(dev_input);
-    // Allocate GPU space for the output
+    // 为输出数据分配 GPU 空间
     cudaMalloc(&dev_output, output_size);
 
-    // Run the reducer kernel
+    // 运行 reduce 内核
     cudaReduce(dev_pairs, dev_output);
 
-    // Allocate space on the host for the output array and copy the data to it
+    // 将输出数据复制回主机
     cudaMemcpy(output, dev_output, output_size, cudaMemcpyDeviceToHost);
 
-    // Free GPU memory for the key/value pairs and output array
+    // 释放 GPU 上的内存
     cudaFree(dev_pairs);
     cudaFree(dev_output);
 }
 
-/*
- * Function to call the cuda map kernel and ensure no errors occur
- */
-void cudaMap(input_type *input, KeyValuePair *pairs) {
+// 调用 map 内核并检查错误
+void cudaMap(input_type *input, Pair *pairs) {
     mapKernel<<<GRID_SIZE, BLOCK_SIZE>>>(input, pairs);
     gpuErrChk( cudaPeekAtLastError() );
     gpuErrChk( cudaDeviceSynchronize() );
 }
 
-/*
- * Function to call the cuda reduce kernel and ensure no errors occur
- */
-void cudaReduce(KeyValuePair *pairs, output_type *output) {
+// 调用 reduce 内核并检查错误
+void cudaReduce(Pair *pairs, output_type *output) {
     reduceKernel<<<GRID_SIZE, BLOCK_SIZE>>>(pairs, output);
     gpuErrChk( cudaPeekAtLastError() );
     gpuErrChk( cudaDeviceSynchronize() );
