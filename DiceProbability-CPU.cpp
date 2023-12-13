21abf6f6a3237af36b5edae891a909fa173b073b
Jiayi(Kevin) Liu <56602568+Thewarmsunshine@users.noreply.github.com>
2023-12-13 00:09:24 -0800

    Update DiceProbability-CPU.cpp

--- DiceProbability-CPU.cpp
+++ DiceProbability-CPU.cpp
@@ -4,27 +4,29 @@
 #include <ctime>
 #include <chrono>
 
+using namespace std;
+
 struct Pair {
     int value;  // 骰子面
 };
 
 // 生成随机骰子面的函数（mapper）
-void mapper(std::vector<Pair>& pairs) {
-    static std::mt19937 rng(static_cast<unsigned int>(time(nullptr)));
-    std::uniform_int_distribution<std::mt19937::result_type> dist(1, 6);
+void mapper(vector<Pair>& pairs) {
+    static mt19937 rng(static_cast<unsigned int>(time(nullptr)));
+    uniform_int_distribution<mt19937::result_type> dist(1, 6);
     for (auto& pair : pairs) {
         pair.value = dist(rng);
     }
 }
 
 // 统计概率的函数（reducer）
-std::vector<float> reducer(const std::vector<Pair>& pairs, int numFaces) {
-    std::vector<int> counts(numFaces, 0);
+vector<float> reducer(const vector<Pair>& pairs, int numFaces) {
+    vector<int> counts(numFaces, 0);
     for (const auto& pair : pairs) {
         counts[pair.value - 1]++;
     }
 
-    std::vector<float> probabilities;
+    vector<float> probabilities;
     for (int count : counts) {
         probabilities.push_back(static_cast<float>(count) / pairs.size());
     }
@@ -33,21 +35,21 @@ std::vector<float> reducer(const std::vector<Pair>& pairs, int numFaces) {
 
 int main() {
     const int numInputs = 100000000;
-    std::vector<Pair> pairs(numInputs);
+    vector<Pair> pairs(numInputs);
 
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = chrono::high_resolution_clock::now();
 
     mapper(pairs);
-    std::vector<float> probabilities = reducer(pairs, 6);
+    vector<float> probabilities = reducer(pairs, 6);
 
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = end - start;
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
 
     for (int i = 0; i < 6; i++) {
-        std::cout << "Probability of Face " << i + 1 << ": " << probabilities[i] * 100 << "%\n";
+        cout << "Probability of Face " << i + 1 << ": " << probabilities[i] * 100 << "%\n";
     }
 
-    std::cout << "Time taken: " << elapsed.count() << " seconds";
+    cout << "Time taken: " << elapsed.count() << " seconds";
 
     return 0;
 }
