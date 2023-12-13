0fdd451c1ae48c1919410fc8455bb369c9dcd63f
Jiayi(Kevin) Liu <56602568+Thewarmsunshine@users.noreply.github.com>
2023-12-12 23:49:04 -0800

    Update config.cuh

--- config.cuh
+++ config.cuh
@@ -1,35 +1,36 @@
 #ifndef MAP_REDUCE_CUH
 #define MAP_REDUCE_CUH
 
-// Configure GPU parameters
-#define GRID_SIZE 1024
-#define BLOCK_SIZE 1024
+// 配置 GPU 参数
+#define GRID_SIZE 1024 // 定义网格大小
+#define BLOCK_SIZE 1024 // 定义块大小
 
-// Set number of input elements, number of output elements, and number of keys
-// per input element
-#define NUM_INPUT 10000000
-#define NUM_OUTPUT 1
-#define NUM_KEYS 1
+// 设置输入元素的数量，输出元素的数量和每个输入元素的键值对数量
+#define NUM_INPUT 10000000 // 定义输入元素的数量
+#define NUM_OUTPUT 6  // 定义输出元素的数量（这里设为6，因为骰子有6个面）
 
-// Example of custom input type
+
+// 自定义输入类型的示例
 struct Coordinate {
-    float x;
-    float y;
+    float x;   // x坐标
+    float y;   // y坐标
 };
 
-// Setting input, output, key, and value types
-typedef Coordinate input_type;
-typedef float output_type;
-typedef char key_type;
-typedef float value_type;
+// 设置输入、输出、键和值的类型
+typedef Coordinate input_type; // 输入类型
+typedef float output_type; // 输出类型（用于存储概率等浮点数）
+typedef float value_type;  // 值的类型
 
-// Do not edit below this line
 
-struct KeyValuePair {
-   key_type key;
-   value_type value;
+struct Pair {
+   value_type value; // 仅包含值的结构，用于MapReduce操作中的值传递
+   // 定义操作符 '<' 用于比较两个 Pair 对象
+    __host__ __device__
+    bool operator<(const Pair& other) const {
+        return value < other.value;
+    }
 };
 
-void runMapReduce(input_type *input, output_type *output);
+void runMapReduce(input_type *input, output_type *output); // 运行 MapReduce 作业的函数
 
-#endif
+#endif // MAP_REDUCE_CUH
